42f62aad5bd5c21bc4909d5a3b787947e1298149
Fabio Lapuinka <lapuinkafabio@gmail.com>
2026-04-29 18:52:05 -0300

    bk

---
+++ Arquivos/2_FUNDAMENTOS/10_abstracao_input/index.js
@@ -0,0 +1,24 @@
+const inquirer = require('inquirer').default;
+
+
+inquirer.prompt([
+    {
+        type: 'input',
+        name: 'p1',
+        message: 'Qual a primeira nota?'
+    }, 
+    {
+        type: 'input',
+        name: 'p2',
+        message: 'Qual a segunda nota?'
+    }
+]).then(answers => {   
+    console.log(answers);
+    const media = (parseFloat(answers.p1) + parseFloat(answers.p2)) / 2;
+    console.log(`A média é: ${media}`);
+
+
+
+}).catch(error => {
+    console.error('Ocorreu um erro:', error);
+});
--- Arquivos/2_FUNDAMENTOS/9_lendo_input/index.js
+++ Arquivos/2_FUNDAMENTOS/9_lendo_input/index.js
@@ -4,6 +4,20 @@ const readline = require("node:readline/promises").createInterface({
 });
 
 readline.question("Qual a sua linguagem preferida? ").then((linguagem) => {
+
+    if(linguagem.toLowerCase() === 'javascript'){
+        console.log('Ótima escolha! JavaScript é uma linguagem muito popular e versátil.');
+    }
+    else if(linguagem.toLowerCase() === 'python'){
+        console.log('Python é uma excelente linguagem para iniciantes e tem uma sintaxe muito clara.');
+    }
+    else if(linguagem.toLowerCase() === 'java'){
+        console.log('Java é uma linguagem robusta e amplamente utilizada em desenvolvimento de aplicativos empresariais.');
+    }
+    else{
+        console.log('Linguagem não reconhecida.');
+    }
     console.log(`Sua linguagem preferida é: ${linguagem}`);
     readline.close();
 });
+
