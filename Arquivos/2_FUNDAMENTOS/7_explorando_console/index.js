98887d038ba1c06348084c0a91b3ecafd8b0eb47
Fabio Lapuinka <lapuinkafabio@gmail.com>
2026-04-29 16:38:22 -0300

    bk

---
+++ Arquivos/2_FUNDAMENTOS/7_explorando_console/index.js
@@ -0,0 +1,16 @@
+const x = 10;
+const y = "João";
+const z = [1,2];
+
+console.log(x,y,z);
+console.count(`valor de x é: ${x}, ddd`);
+console.count(`valor de x é: ${x}, ddd`);
+console.count(`valor de x é: ${x}, ddd`);
+console.count(`valor de x é: ${x}, ddd`);
+
+console.log("O nome é %s, ele é programador",y);
+
+setTimeout(() => {
+    console.log('Isso é um timeout');
+    console.clear();
+}, 2000);
