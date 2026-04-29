3b95814bb092bdc1f935364d544e9ac3e08a99d7
Fabio Lapuinka <lapuinkafabio@gmail.com>
2026-04-29 19:36:14 -0300

    bk

---
+++ Arquivos/2_FUNDAMENTOS/12_event_emitter/index.js
@@ -0,0 +1,11 @@
+const EventEmitter = require('events');
+
+const emitter = new EventEmitter();
+
+emitter.on('start', (name) => {
+    console.log(`Olá, ${name}! Bem-vindo ao mundo dos eventos!`);
+});
+
+console.log('Antes');
+emitter.emit('start', 'Fabio'); 
+console.log('Depois');
