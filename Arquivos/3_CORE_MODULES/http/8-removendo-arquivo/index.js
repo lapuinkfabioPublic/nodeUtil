16775bf0c26cb6c7144381efee382a1ecefb3a88
Fabio Lapuinka <lapuinkafabio@gmail.com>
2026-04-30 14:28:13 -0300

    bk

--- Arquivos/3_CORE_MODULES/http/8-removendo-arquivo/index.js
+++ Arquivos/3_CORE_MODULES/http/8-removendo-arquivo/index.js
@@ -5,6 +5,7 @@ fs.unlink('name.txt', (err) => {
 
     if (err) {
         console.error('Erro ao remover o arquivo:', err);   
+        return;
     } else {
         console.log('Arquivo removido com sucesso!');
     }   
