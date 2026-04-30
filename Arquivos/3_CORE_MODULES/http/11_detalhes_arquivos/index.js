acccea71be7cdf6307a677bb5c74edbbed1a4c2a
Fabio Lapuinka <lapuinkafabio@gmail.com>
2026-04-30 17:00:45 -0300

    bk

--- Arquivos/3_CORE_MODULES/http/11_detalhes_arquivos/index.js
+++ Arquivos/3_CORE_MODULES/http/11_detalhes_arquivos/index.js
@@ -6,6 +6,11 @@ fs.stat('arquivo.txt', (err, stats) => {
         console.error('Erro ao obter detalhes do arquivo:', err);
         return;
     }   
+    console.log('Detalhes do arquivo:');
+    console.log(stats.isDirectory() ? 'É um diretório' : 'É um arquivo');
+    console.log(stats.isBlockDevice() ? 'É um dispositivo de bloco' : 'Não é um dispositivo de bloco');
+    console.log(stats.isCharacterDevice() ? 'É um dispositivo de caractere' : 'Não é um dispositivo de caractere');
+    console.log(stats.isSymbolicLink() ? 'É um link simbólico' : 'Não é um link simbólico');
     console.log('Tamanho do arquivo:', stats.size, 'bytes');
     console.log('Data de criação:', stats.birthtime);
     console.log('Data da última modificação:', stats.mtime);
