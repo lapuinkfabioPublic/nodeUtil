634aa56d25088fd347fb67ce08695fc297cb90db
Fabio Lapuinka <lapuinkafabio@gmail.com>
2026-04-30 16:42:01 -0300

    bk

--- Arquivos/3_CORE_MODULES/http/10_roteamento/index.js
+++ Arquivos/3_CORE_MODULES/http/10_roteamento/index.js
@@ -1,3 +1,8 @@
+//O Codigo pode ser feito com ajuda de A.I mais tem q ser Clean para Humanos darem manutenção depois, 
+// e o mais importante é que o codigo tem q ser funcional, 
+// ou seja, tem q rodar sem erros, e tem q ser facil de entender para outros programadores 
+
+
 const http = require('http');
 const fs = require('fs');
 const url = require('url');
@@ -26,6 +31,7 @@ server.listen(port, () => {
 
 
 
+// que possam trabalhar no projeto depois.
 //Realiza a Leitura de um Arquivo
 function lerArquivo(res, filename) {
     
@@ -40,16 +46,21 @@ function lerArquivo(res, filename) {
             }); 
         }
         else {
-            res.writeHead(404, { 'Content-Type': 'text/html' });
-            res.write('<h1>404 Not Found</h1>');
-            return res.end();
+            PaginaNaoEncontrada(res);
         }
     }
     else{
 
-         res.writeHead(404, { 'Content-Type': 'text/html' });
-         res.write('<h1>404 Not Found</h1>');
-         return res.end();
+          PaginaNaoEncontrada(res);
     }
 
+}
+
+function PaginaNaoEncontrada(res) {
+
+    fs.readFile('404.html', (err, data) => {
+        res.writeHead(404, { 'Content-Type': 'text/html' });
+        res.write(data);
+        return res.end();       
+    } );    
 }
