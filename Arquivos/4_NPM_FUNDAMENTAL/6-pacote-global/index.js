0193af4ad2322439d6fb2fc9b7f52d2929679476
Fabio Lapuinka <lapuinkafabio@gmail.com>
2026-04-30 22:58:19 -0300

    bk

---
+++ Arquivos/4_NPM_FUNDAMENTAL/6-pacote-global/index.js
@@ -0,0 +1,6 @@
+const _ = require('lodash');
+
+const arr =  [1, 2, 3, 3, 4, 4, 5, 6, 7, 8, 9, 10];
+
+console.log(_.uniq(arr)); // [1, 2, 3, 4, 5, 6, 7, 8, 9, 10] - remove os elementos duplicados do array      
+
