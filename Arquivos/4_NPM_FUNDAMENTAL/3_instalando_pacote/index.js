e29a9ea9d649e7b27d9ef15f0313cb33493c773f
Fabio Lapuinka <lapuinkafabio@gmail.com>
2026-04-30 21:06:36 -0300

    bk

---
+++ Arquivos/4_NPM_FUNDAMENTAL/3_instalando_pacote/index.js
@@ -0,0 +1,9 @@
+const _ = require('lodash'); //como o Jquery $ o _ é o lodash, uma biblioteca de utilidades para JavaScript
+
+const a = [1, 2, 3, 4, 5];
+const b = [4, 5, 6, 7, 8];
+
+const diff = _.difference(a, b); //diferença entre os arrays a e b
+console.log(diff); // [1, 2, 3] - elementos que estão em a mas não estão em b
+
+
