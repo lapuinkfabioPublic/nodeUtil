27a398cf207dbe4f7abe476a35d887e9b2da3bf5
Fabio Lapuinka <lapuinkafabio@gmail.com>
2026-05-01 23:31:50 -0300

    bk

--- Arquivos/6_EXPRESS/2_setup/index.js
+++ Arquivos/6_EXPRESS/2_setup/index.js
@@ -7,3 +7,4 @@ app.get('/', (req, res) => {
 app.listen(3000, () => {
     console.log('Servidor rodando na porta 3000');
 }); 
+    
--- Arquivos/6_EXPRESS/3_render_html/index.js
+++ Arquivos/6_EXPRESS/3_render_html/index.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const app = express();
+const path = require('path');
+const basePath = path.join(__dirname, 'templates');
 
 app.get('/', (req, res) => {
-    res.send('Hello World!');
+    res.sendFile(path.join(basePath, 'index.html'));
 });
 app.listen(3000, () => {
     console.log('Servidor rodando na porta 3000');
 }); 
+    
