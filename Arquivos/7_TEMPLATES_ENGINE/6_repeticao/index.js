c633a71b91a005fc793c3f938fb323e21d06d0d7
Fabio Lapuinka <lapuinkafabio@gmail.com>
2026-05-02 21:51:06 -0300

    bk

---
+++ Arquivos/7_TEMPLATES_ENGINE/6_repeticao/index.js
@@ -0,0 +1,58 @@
+const express = require('express');
+const exphbs = require('express-handlebars');
+const app = express();
+
+app.engine('handlebars', exphbs.engine({
+    defaultLayout: 'main', // Especifica el layout predeterminado
+    layoutsDir: __dirname + '/views/layouts' // Directorio de los layouts
+})) ;
+
+app.set('view engine', 'handlebars');
+
+app.get("/dashboard", (req, res) => {
+    const user = {
+        name: 'João',
+        surname: 'Silva',
+        age: 30,
+        city: 'São Paulo'
+    };
+    const auth = true;
+
+    const items = ['1', '2', '3', '4'];
+
+
+
+    res.render('dashboard', { user: user, auth: auth ,  items: items   });
+});
+
+app.get('/', (req, res) => {
+    const user = {
+        name: 'João',
+        surname: 'Silva',
+        age: 30,
+        city: 'São Paulo'
+    };
+
+    const auth = true
+    const palavra = 'Ouro e Prata';
+    res.render('home', { user : user , palavra : palavra, auth: auth});
+});
+app.get('/contact', (req, res) => {
+    res.render('contact');
+});
+
+app.get('/contact/:id', (req, res) => {
+    const id = req.params.id;
+    res.render('contact', { id: id });
+  
+});
+
+app.get('/about', (req, res) => {
+    res.render('about');
+});
+
+app.listen(3000, () => {
+    console.log('Servidor rodando na porta 3000');
+});
+
+
