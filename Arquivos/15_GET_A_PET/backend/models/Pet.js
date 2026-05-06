75e3c28fd7d8b81f30a352f0ad2474d12c4d556e
Fabio Lapuinka <lapuinkafabio@gmail.com>
2026-05-05 21:26:43 -0300

    bk

---
+++ Arquivos/15_GET_A_PET/backend/models/Pet.js
@@ -0,0 +1,43 @@
+const mongoose = require('../db/conn')
+const { Schema } = mongoose
+
+
+const User = mongoose.model(
+
+    'Pet',
+    new Schema({
+        name: {
+            type: String,
+            required
+        },
+
+        age:{
+            type: Number,
+            required: true
+        },
+
+        weight: {
+            type: Number,
+            required: true
+        },
+
+        color: {
+            type: String,
+            required: true
+        },
+
+        images: {
+            type: Array,
+            required: true
+
+        },
+        available: {
+            type: Boolean
+        },
+        user:Object,
+        adopter: Object
+    },
+    {timestamps: true} ,
+  )
+)
+module.exports = Pet
