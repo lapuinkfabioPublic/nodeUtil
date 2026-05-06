cc435da43601e9f38f7cf4d3f5c3c49a8b55a46c
Fabio Lapuinka <lapuinkafabio@gmail.com>
2026-05-06 20:46:52 -0300

    bk

--- Arquivos/15_GET_A_PET/backend/controllers/UserController.js
+++ Arquivos/15_GET_A_PET/backend/controllers/UserController.js
@@ -4,6 +4,17 @@ module.exports = class UserController
 {
     static async register(req, res)
     {
+
+       /* const name = req.body.name
+        const email = req.body.email
+        
+        const phone = req.body.phone
+        const password = req.body.password
+        const confirmpassword = req.body.confirmpassword
+        */
+
+
+        const {name, email, phone, password, confirmpassword} = req.body
         res.json('Olá Get A Pet')
        // res.end
     }
