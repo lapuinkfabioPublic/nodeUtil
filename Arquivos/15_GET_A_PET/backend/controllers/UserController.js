67f045d3da771f18a6ce66e2dde18524705a6e95
Fabio Lapuinka <lapuinkafabio@gmail.com>
2026-05-06 21:09:28 -0300

    bk

--- Arquivos/15_GET_A_PET/backend/controllers/UserController.js
+++ Arquivos/15_GET_A_PET/backend/controllers/UserController.js
@@ -15,6 +15,35 @@ module.exports = class UserController
 
 
         const {name, email, phone, password, confirmpassword} = req.body
+
+
+        //validations
+
+        if(!name){
+
+            res.status(422).json({message: 'O nome é obrigatório'})
+            return
+        }
+        if(!email){
+
+            res.status(422).json({message: 'O email é obrigatório'})
+            return
+        }
+        if(!phone){
+
+            res.status(422).json({message: 'O phone é obrigatório'})
+            return
+        }
+        if(!password){
+
+            res.status(422).json({message: 'O password é obrigatório'})
+            return
+        }
+        if(!confirmpassword){
+
+            res.status(422).json({message: 'O confirmpassword é obrigatório'})
+            return
+        }
         res.json('Olá Get A Pet')
        // res.end
     }
