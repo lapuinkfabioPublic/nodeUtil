c4f51629139f5fc505c32f98b158e14b1996f1e7
Fabio Lapuinka <lapuinkafabio@gmail.com>
2026-05-07 22:29:01 -0300

    bk

---
+++ Arquivos/9_sequelize/4_lendo_dados/index.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const { engine } = require('express-handlebars');
+const conn = require('./db/conn');
+const User = require('./models/User');
+const app = express();
+
+app.use(
+    express.urlencoded({extended: true}
+));
+
+app.use(express.json());
+
+app.engine('handlebars', engine());
+app.set('view engine', 'handlebars');
+app.use(express.static('public'));
+
+app.post('/users/create', async (req, res) => {
+    const name = req.body.name;
+    const occupation = req.body.occupation;
+    let newsletter = req.body.newsletter;
+    console.log('aqui');
+    if(newsletter === 'on') {
+        newsletter = true;
+    } else {
+        newsletter = false;
+    }
+
+    console.log(req.body);
+    await User.create({name, occupation, newsletter});
+    res.redirect('/');
+});
+
+
+app.get('/users/create',  (req, res) => {
+    res.render('adduser');
+});
+
+
+app.get('/', async (req, res)  => {
+  
+    const users = await User.findAll({raw:true});
+    console.log(users);
+    res.render('home', {users: users});    
+    
+});
+
+app.listen(3001, () => {
+    console.log('Servidor rodando na porta 3000');
+});
