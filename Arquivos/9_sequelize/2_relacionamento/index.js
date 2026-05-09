3f5c806dd0b0ced56b0973ceb4cf38ca39b0a730
Fabio Lapuinka <lapuinkafabio@gmail.com>
2026-05-09 00:27:57 -0300

    bk

---
+++ Arquivos/9_sequelize/2_relacionamento/index.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const { engine } = require('express-handlebars');
+const conn = require('./db/conn');
+const User = require('./models/User');
+const app = express();
+
+app.use(
+    express.urlencoded({extended: true}
+));
+
+app.use(express.json());
+
+app.engine('handlebars', engine());
+app.set('view engine', 'handlebars');
+app.use(express.static('public'));
+
+app.post('/users/create', async (req, res) => {
+    const name = req.body.name;
+    const occupation = req.body.occupation;
+    let newsletter = req.body.newsletter;
+    console.log('aqui');
+    if(newsletter === 'on') {
+        newsletter = true;
+    } else {
+        newsletter = false;
+    }
+
+    console.log(req.body);
+    await User.create({name, occupation, newsletter});
+    res.redirect('/');
+});
+
+
+app.get('/users/create',  (req, res) => {
+    res.render('adduser');
+});
+
+
+app.get('/', (req, res) => {
+    res.render('home');
+});
+
+app.listen(3000, () => {
+    console.log('Servidor rodando na porta 3000');
+});
