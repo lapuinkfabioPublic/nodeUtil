6fad1a3467a75540f916dc74775a8c7e3b225171
Fabio Lapuinka <lapuinkafabio@gmail.com>
2026-05-08 22:59:37 -0300

    bk

--- Arquivos/9_sequelize/4_lendo_dados/index.js
+++ Arquivos/9_sequelize/4_lendo_dados/index.js
@@ -14,8 +14,9 @@ app.engine('handlebars', engine());
 app.set('view engine', 'handlebars');
 app.use(express.static('public'));
 
-app.post('/users/delete:id', async (req, res) => {
-    const id = req.body.id;
+
+app.post('/users/delete/:id', async (req, res) => {
+    const id = req.params.id;
     await
         User.destroy({where: {id: id}});
     res.redirect('/');
@@ -36,6 +37,29 @@ app.post('/users/create', async (req, res) => {
 
     console.log(req.body);
     await User.create({name, occupation, newsletter});
+    res.redirect('/');
+});
+
+
+app.get('/users/edit/:id', async (req, res) => {
+    const id = req.params.id;
+    const user = await User.findOne({raw: true, where: {id: id}});
+    res.render('edituser', {user});
+});
+
+//Roda de atualizacao
+app.post('/users/update/:id', async (req, res) => {
+    const id = req.params.id;
+    const name = req.body.name;
+    const occupation = req.body.occupation;
+    let newsletter = req.body.newsletter;   
+    if(newsletter === 'on') {
+        newsletter = true;
+    }
+    else {
+        newsletter = false;
+    }
+    await User.update({name, occupation, newsletter}, {where: {id: id}});
     res.redirect('/');
 });
 
