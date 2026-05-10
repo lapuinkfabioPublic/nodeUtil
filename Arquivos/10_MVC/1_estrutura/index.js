dcf80c6a92a8ec694a6263bb534a0dd1e985c0a2
Fabio Lapuinka <lapuinkafabio@gmail.com>
2026-05-10 20:04:14 -0300

    bk

--- Arquivos/10_MVC/1_estrutura/index.js
+++ Arquivos/10_MVC/1_estrutura/index.js
@@ -1,9 +1,19 @@
 const express = require('express');
+const exphbs = require('express-handlebars');
 const app = express();
-app.get('/', (req, res) => {
-    res.send('Hello World!');
-});
-app.listen(3000, () => {
-    console.log('Example app listening on port 3000!');
-});
+app.engine('handlebars', exphbs.engine());
+app.set('view engine', 'handlebars');
+const conn = require('./db/conn');
 
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.render('home');
+}); 
+
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+});
