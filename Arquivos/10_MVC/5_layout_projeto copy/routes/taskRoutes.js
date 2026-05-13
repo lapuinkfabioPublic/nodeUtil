37787ab1f3589e13e24177065df160e982abad09
Fabio Lapuinka <lapuinkafabio@gmail.com>
2026-05-12 22:41:55 -0300

    bk

--- Arquivos/10_MVC/5_layout_projeto copy/routes/taskRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const TaskController = require('../controllers/TaskController');
-
-const router = express.Router();
-router.get('/', TaskController.showTasks);
-router.get('/add', TaskController.createTask); 
-module.exports = router;
