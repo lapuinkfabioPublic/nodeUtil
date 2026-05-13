6ce60c5d863cd45722aaa0f28b3cef412e83811c
Fabio Lapuinka <lapuinkafabio@gmail.com>
2026-05-12 22:22:58 -0300

    bk

--- Arquivos/10_MVC/5_layout_projeto/controllers/TaskController.js
+++ Arquivos/10_MVC/5_layout_projeto/controllers/TaskController.js
@@ -19,4 +19,40 @@ module.exports = class TaskController {
     static async createTask(req, res) {
         res.render('tasks/create'); 
     }
+
+    static async toggleTaskDone(req, res) {
+        const id = req.params.id;
+        const task = await Task.findByPk(id);   
+        if (task) {
+            task.done = !task.done;
+            await task.save();
+        }
+
+        res.redirect('/tasks');
+    }
+
+    static async deleteTask(req, res) {
+        const id = req.params.id;
+        await Task.destroy({ where: { id } });
+        res.redirect('/tasks');
+    }
+
+    static async editTask(req, res) {
+        const id = req.params.id;
+        const task = await Task.findByPk(id, { raw: true });
+
+        if (task) {
+            res.render('tasks/edit', { task });
+        }   
+    }
+
+    static async editTaskSave(req, res) {
+        const id = req.body.id;
+        const task = {
+            title: req.body.title,
+            description: req.body.description
+        };      
+        await Task.update(task, { where: { id } });
+        res.redirect('/tasks');
+    }
 }
--- Arquivos/10_MVC/5_layout_projeto/routes/taskRoutes.js
+++ Arquivos/10_MVC/5_layout_projeto/routes/taskRoutes.js
@@ -5,4 +5,8 @@ const router = express.Router();
 router.get('/', TaskController.showTasks);
 router.get('/add', TaskController.createTask);
 router.post('/add', TaskController.createTaskSave);
+router.get('/toggle/:id', TaskController.toggleTaskDone);
+router.get('/delete/:id', TaskController.deleteTask);
+router.get('/edit/:id', TaskController.editTask);
+router.post('/edit/:id', TaskController.editTaskSave);
 module.exports = router;
